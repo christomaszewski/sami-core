bd2c7bb9178c90aa0d251dde643fec0939a90196
jjblum <jason.j.blum@gmail.com>
2015-07-31 19:44:26 -0400

    Small updates to support new Madara-based sami-crw.

--- src/sami/proxy/ProxyServerInt.java
+++ src/sami/proxy/ProxyServerInt.java
@@ -38,6 +38,8 @@ public interface ProxyServerInt {
 
     public int getProxyCounter();
     
+    public int nextHighestBoatNo();
+    
     public void printKB();
     //////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
